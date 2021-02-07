c10ea970a00b162ed6a0f51199957fbf6d930267
JaitriSaiMeenakshi <73230572+JaitriSaiMeenakshi@users.noreply.github.com>
2021-02-07 18:58:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video= createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+song = "";
+
+function preload()
+{
+    song = loadSound("sorry.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+
+
+}
+
+function play()
+{
+    song.play();
+}
